72a028377910838f59437d4767e26748a76f6baf
kingproxj <48118067+kingproxj@users.noreply.github.com>
2019-11-28 20:46:29 +0800

    Create test.py

---
+++ Python3.7/test.py
@@ -0,0 +1,22 @@
+import os
+import urllib
+
+os.putenv('codeUri','https://raw.githubusercontent.com/kingproxj/fcs/master/index.py,https://raw.githubusercontent.com/kingproxj/fcs/master/Python3.7/test.py')
+
+def Schedule(blocknum, blocksize, totalsize):
+    '''
+    :param blocknum: 已经下载的数据块
+    :param blocksize: 数据块的大小
+    :param totalsize: 远程文件的大小
+    :return:
+    '''
+    per = 100.0*blocknum*blocksize/totalsize
+    if per > 100:
+        per = 100
+        print('当前下载进度：%d') % per
+
+codeUri = os.getenv('codeUri')
+for code_url in codeUri:
+    filename = code_url.split('/')[-1]
+	  print("开始下载%s", filename)
+    urllib.request.urlretrieve(code_url, filename, Schedule)		
