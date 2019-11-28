c0bd106403b666710058d9943546b4facd8c08b2
kingproxj <48118067+kingproxj@users.noreply.github.com>
2019-11-28 14:28:08 +0800

    Update urllib.py

--- Python3.7/urllib.py
+++ Python3.7/urllib.py
@@ -1,6 +1,6 @@
 import urllib
 
-print "downloading with urllib"
+print("downloading with urllib")
 url = 'https://raw.githubusercontent.com/kingproxj/fcs/master/index.py'
-print "downloading with urllib"
+print("downloading with urllib")
 urllib.urlretrieve(url, "index-urllib.py")
