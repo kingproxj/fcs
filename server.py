12c44c13a02860ade38a6593ed97d8acbd840b00
kingproxj <48118067+kingproxj@users.noreply.github.com>
2019-12-11 11:53:59 +0800

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@
 # 从wsgiref模块导入:
 from wsgiref.simple_server import make_server
 # 导入我们自己编写的application函数:
-from index import application
+from HandlerName import FunctionName
 
 # 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
 httpd = make_server('', 8000, application)
