f133c80b5fbbc4c190788ac9c17f20416c148216
kingproxj <48118067+kingproxj@users.noreply.github.com>
2019-12-11 11:53:10 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,6 +1,5 @@
-export handler=index.application
-myhandler=$(echo $handler | cut -d"." -f1)
-myfunction=$(echo $handler | cut -d"." -f2)
+myhandler=$(echo $Handler | cut -d"." -f1)
+myfunction=$(echo $Handler | cut -d"." -f2)
 echo $myhandler $myfunction
 handlerstr=$(echo 's/HandlerName/'$myhandler'/')
 echo $handlerstr
